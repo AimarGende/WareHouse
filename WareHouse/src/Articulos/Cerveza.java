23edaf2957badd1cfdc1bb5fe11ae9d031ff2370
Usuario <Usuario@DESKTOP-NRMM62F>
2023-01-03 17:13:55 +0100

    getters y setters

--- WareHouse/src/Articulos/Cerveza.java
+++ WareHouse/src/Articulos/Cerveza.java
@@ -17,7 +17,33 @@ public class Cerveza extends Articulo {
 		super();
 	}
 
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public String getCereales() {
+		return cereales;
+	}
+
+	public void setCereales(String cereales) {
+		this.cereales = cereales;
+	}
+
+	public double getGradosAlcohol() {
+		return gradosAlcohol;
+	}
+
+	public void setGradosAlcohol(double gradosAlcohol) {
+		this.gradosAlcohol = gradosAlcohol;
+	}
 	
+	
+
+
 	
 	
 }
