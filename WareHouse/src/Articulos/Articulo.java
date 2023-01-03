e48ef5c162a9445c2a37573036448fe1bc6aef4e
Usuario <Usuario@DESKTOP-NRMM62F>
2023-01-03 15:33:42 +0100

    Articulos atributos

---
+++ WareHouse/src/Articulos/Articulo.java
@@ -0,0 +1,10 @@
+package Articulos;
+
+public abstract class Articulo {
+	private String code;
+	private String nombre;
+	private String marca;
+	private int capacidadBotella;
+	private double precio;
+	private int stock;
+}
