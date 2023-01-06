85db830b7195df43f61018caf211619a6d026a59
Usuario <Usuario@DESKTOP-NRMM62F>
2023-01-06 18:48:48 +0100

    Constructor y atributos

---
+++ WareHouse/src/Almacen/Almacen.java
@@ -0,0 +1,13 @@
+package Almacen;
+import Articulos.*;
+import java.util.ArrayList;
+public class Almacen {
+	ArrayList<Articulo> articuloList;
+	public Almacen(){
+		articuloList=new ArrayList<Articulo>();
+	}
+	
+	public void cargarDatos() {
+		
+	}
+}
