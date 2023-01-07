965a145fbc172ed782e5129cd4d0e1e589a7ed6f
Usuario <Usuario@DESKTOP-NRMM62F>
2023-01-07 15:32:26 +0100

    Menu acabado

--- WareHouse/src/Almacen/GestorAlmacenApp.java
+++ WareHouse/src/Almacen/GestorAlmacenApp.java
@@ -1,9 +1,12 @@
 package Almacen;
 import Almacen.*;
+
+import java.io.IOException;
 import java.util.Scanner;
+import Factura.*;
 public class GestorAlmacenApp {
 
-	public static void run() {
+	public static void run() throws IOException {
 		final int REALIZAR_VENTA=1;
 		final int REALIZAR_COMPRA=2;
 		final int VER_ARTICULOS_SALUDABLES=3;
@@ -11,14 +14,30 @@ public class GestorAlmacenApp {
 		final int VER_ARTICULO_CON_MENOS_STOCK=5;
 		final int SALIR=0;
 		
+		Scanner sc=new Scanner(System.in);
 		Almacen almacen=new Almacen();
+		Factura factura=new Factura();
 		int opcion_menu=0;
 		
 		do {
+			System.out.println("----------MENU----------");
+			System.out.println(REALIZAR_VENTA+". realizar venta");
+			System.out.println(REALIZAR_COMPRA+". realizar compra");
+			System.out.println(VER_ARTICULOS_SALUDABLES+". ver articulos saludables");
+			System.out.println(VER_ARTICULO_MAS_CARO+". ver articulo mas caro");
+			System.out.println(VER_ARTICULO_CON_MENOS_STOCK+". ver articulo con menos stock que el indicado");
+			System.out.println(SALIR+". salir");
+			System.out.println("Escoge la opcion que quieras:");
+			opcion_menu=Integer.parseInt(sc.nextLine());
 			switch(opcion_menu) {
 				case REALIZAR_VENTA:
+					System.out.println("Mostrando factura...");
+					factura.mostrarEnPantalla();
+					System.out.println("Guardando factura...");
+					factura.guardarEnFichero();
 					break;
 				case REALIZAR_COMPRA:
+					
 					break;
 				case VER_ARTICULOS_SALUDABLES:
 					break;
@@ -32,5 +51,6 @@ public class GestorAlmacenApp {
 					break;
 			}
 		}while(opcion_menu!=SALIR);
+		sc.close();
 	}
 }
