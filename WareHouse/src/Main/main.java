608b8cfd35a6e36854d9a90dbb54bb6f7a2b55ea
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-10 09:35:42 +0100

    hecho

---
+++ WareHouse/src/Main/main.java
@@ -0,0 +1,13 @@
+package Main;
+import java.io.IOException;
+import java.text.ParseException;
+
+import Almacen.*;
+public class main {
+
+	public static void main(String[] args) throws IOException, ParseException {
+		GestorAlmacenApp.run();
+
+	}
+
+}
