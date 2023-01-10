bb409c93cd589f1eb964aca170ddf4af93d4ce7f
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-10 09:09:08 +0100

    boolean arreglado

--- WareHouse/src/Almacen/Almacen.java
+++ WareHouse/src/Almacen/Almacen.java
@@ -15,11 +15,19 @@ public class Almacen {
 	public void cargarDatos() throws FileNotFoundException {
 		File file=new File("Datos/articulos.txt");
 		Scanner scan=new Scanner(file);
+		boolean zumo=false;
+		boolean gaseoso=false;
 		String[] partes;
 		while(scan.hasNextLine()) {
 			partes=scan.nextLine().split(";");
 			if(partes[2].toLowerCase().equals("refresco")) {
-				Refresco refres=new Refresco(partes[0], partes[1], partes[2], partes[3],Integer.parseInt(partes[4]), Double.parseDouble(partes[5]), Integer.parseInt(partes[6]), partes[7], Boolean.parseBoolean(partes[8]), Boolean.parseBoolean(partes[9]),Integer.parseInt(partes[10]));
+				if(partes[8].toLowerCase().equals("si")) {
+					zumo=true;
+				}
+				if(partes[9].toLowerCase().equals("si")) {
+					gaseoso=true;
+				}
+				Refresco refres=new Refresco(partes[0], partes[1], partes[2], partes[3],Integer.parseInt(partes[4]), Double.parseDouble(partes[5]), Integer.parseInt(partes[6]), partes[7], zumo, gaseoso,Integer.parseInt(partes[10]));
 				articuloList.add(refres);
 			}else if(partes[2].toLowerCase().equals("vino")) {
 				Vino vin=new Vino(partes[0], partes[1], partes[2], partes[3],Integer.parseInt(partes[4]), Double.parseDouble(partes[5]), Integer.parseInt(partes[6]), partes[7], partes[8], Integer.parseInt(partes[9]), partes[10], Double.parseDouble(partes[11]));
