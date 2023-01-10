42948fd5d0b03ab0cddd0e7c20be4026ea93416d
Aimar Gende <Aimar Gende@HZ114821.H012108.NET>
2023-01-10 08:19:16 +0100

    Acabado, probar

---
+++ WareHouse/src/Articulos/ArticuloComparatorPrecio.java
@@ -0,0 +1,22 @@
+package Articulos;
+
+import java.util.Comparator;
+
+public class ArticuloComparatorPrecio implements Comparator<Articulo>{
+
+	@Override
+	public int compare(Articulo o1, Articulo o2) {
+		int i=0;
+		if(o1.getPrecio()<o2.getPrecio()) {
+			i=-1;
+		}
+		if(o1.getPrecio()>o2.getPrecio()) {
+			i=1;
+		}
+		if(o1.getPrecio()==o2.getPrecio()) {
+			i=0;
+		}
+		return i;
+	}
+
+}
